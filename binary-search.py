0208672f0830ac4f56844f352655957da8e5b5e0
emigoh <emi.goh2011@gmail.com>
2023-12-01 15:12:21 -0500

    solved problem and checked with leetcode

--- binary-search.py
+++ binary-search.py
@@ -41,4 +41,17 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        
+        low = 0
+        high = len(nums)-1
+
+        while low <= high:
+            mid = (low + high)//2
+
+            if (target == nums[mid]):
+                return mid
+            elif (target > nums[mid]):
+                low = mid + 1
+            else:                       
+                high = mid - 1
+
+        return -1

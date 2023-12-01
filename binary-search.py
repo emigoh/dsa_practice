08e18d22941149c00915189b43ae439987f78d73
emigoh <emi.goh2011@gmail.com>
2023-12-01 14:48:44 -0500

    pasted starter code

--- binary-search.py
+++ binary-search.py
@@ -22,3 +22,11 @@ Input: nums = [-1,0,3,5,9,12], target = 2
 Output: -1
 Explanation: 2 does not exist in nums so return -1'''
 
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        

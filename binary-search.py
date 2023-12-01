d4c2305798c2a4f01121ee7636655ef542ecef6c
emigoh <emi.goh2011@gmail.com>
2023-12-01 14:47:04 -0500

    examples act as tests

--- binary-search.py
+++ binary-search.py
@@ -9,5 +9,16 @@ Constraints:
 -104 < nums[i], target < 104
 All the integers in nums are unique.
 nums is sorted in ascending order.
-https://leetcode.com/problems/binary-search/'''
+https://leetcode.com/problems/binary-search/
+
+
+Example 1:
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1'''
 
